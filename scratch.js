eb6191ab1fe4098ad392bc6f5285c8f9174abd69
Ansif <99172928+AnsifMkr@users.noreply.github.com>
2026-04-25 21:58:19 +0530

    Delete scratch.js

--- scratch.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const User = require('./backend/models/User');
-
-async function test() {
-  await mongoose.connect('mongodb://localhost:27017/learning-platform'); // Assume standard mongod logic? Actually we don't know the connection string. Let's not guess.
-}
